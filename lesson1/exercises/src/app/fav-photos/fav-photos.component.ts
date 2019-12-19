1939dcfadcb0dd66513ef61b91f3587cb3f7ec9e
Rachel <raitkens724@gmail.com>
2019-12-18 19:17:59 -0600

    part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://github.githubassets.com/images/spinners/octocat-spinner-128.gif';
+  image3 = 'https://repl.it/public/images/logo.svg';
 
   constructor() { }
 
